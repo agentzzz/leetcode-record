7323a35c8788525b3797a6c8113746469e3ce660
zhoujiayang <zhoujiayang@bytedance.com>
2023-10-24 08:55:24 +0800

    update

---
+++ data_structure/LC2678_老人的数目.go
@@ -0,0 +1,23 @@
+package data_structure
+
+import "strconv"
+
+func countSeniors(details []string) int {
+	num := 0
+	for _, detail := range details {
+		age := getAgeFromString(detail)
+		if age > 60 {
+			num++
+		}
+	}
+	return num
+}
+
+func getAgeFromString(s string) int {
+	if len(s) < 13 {
+		return 0
+	}
+	ageStr := s[11:13]
+	age, _ := strconv.ParseInt(ageStr, 10, 32)
+	return int(age)
+}
