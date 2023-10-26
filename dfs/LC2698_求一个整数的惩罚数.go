ac11d3f39ff9786ed5d54f602eea204f0f57497c
zhoujiayang <zhoujiayang@bytedance.com>
2023-10-26 10:26:07 +0800

    update

--- dfs/LC2698_求一个整数的惩罚数.go
+++ dfs/LC2698_求一个整数的惩罚数.go
@@ -1,32 +1,30 @@
 package dfs
 
-//func punishmentNumber(n int) int {
-//	sum := 0
-//	for i := 1; i <= n; i++ {
-//		target := i * i
-//		targetStr := strconv.Itoa(target)
-//		if check(targetStr, target, 0, 0) {
-//			sum += target
-//		}
-//	}
-//	return sum
-//}
-//
-//func check(targetStr string, target, sum, start int) bool {
-//	// 出口
-//	if start == len(targetStr) {
-//		return target == sum
-//	}
-//	// 分支
-//	for i := start; i < len(targetStr); i++ {
-//		temp := 0
-//		temp = temp*10 + int(targetStr[i]-'0')
-//		if sum+temp > target {
-//			break
-//		}
-//		if check(targetStr, target, sum+temp, start+1) {
-//			return true
-//		}
-//	}
-//	return false
-//}
+import "strconv"
+
+func punishmentNumber(n int) int {
+	var check func(string, int, int, int) bool
+	check = func(str string, start, pathSum, target int) bool {
+		if start == len(str) {
+			return pathSum == target
+		}
+		temp := 0
+		for i := start; i < len(str); i++ {
+			temp = temp*10 + int(str[i]-'0')
+			if pathSum+temp > target {
+				break
+			}
+			if check(str, start+1, pathSum+temp, target) {
+				return true
+			}
+		}
+		return false
+	}
+	res := 0
+	for i := 1; i <= n; i++ {
+		if check(strconv.Itoa(i*i), 0, 0, i) {
+			res += i * i
+		}
+	}
+	return res
+}
--- dfs/case_test.go
+++ dfs/case_test.go
@@ -2,38 +2,10 @@ package dfs
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 )
 
 func TestCase(t *testing.T) {
 	sum := punishmentNumber(37)
 	fmt.Println(sum)
-}
-
-func punishmentNumber(n int) int {
-	var check func(string, int, int, int) bool
-	check = func(str string, start, pathSum, target int) bool {
-		if start == len(str) {
-			return pathSum == target
-		}
-		temp := 0
-		for i := start; i < len(str); i++ {
-			temp = temp*10 + int(str[i]-'0')
-			if pathSum+temp > target {
-				break
-			}
-			if check(str, start+1, pathSum+temp, target) {
-				return true
-			}
-		}
-		return false
-	}
-	res := 0
-	for i := 1; i <= n; i++ {
-		if check(strconv.Itoa(i*i), 0, 0, i) {
-			res += i * i
-		}
-	}
-	return res
 }
